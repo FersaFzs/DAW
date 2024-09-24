5981ba075caf0648786e29b96c48be79367a557b
FersaFzs <fersafzs04@gmail.com>
2024-09-24 20:49:54 +0200

    Ejercicios 2 parte 2

--- Ejercicios2/src/Ejercicio8.java
+++ Ejercicios2/src/Ejercicio8.java
@@ -1,3 +1,24 @@
 import java.util.Scanner;
 public class Ejercicio8 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        //Ejercicio 8
+        System.out.println("Introduce tu fecha de nacimiento (Formato: dia mes año)");
+        int dia = scanner.nextInt();
+        int mes = scanner.nextInt();
+        int anio = scanner.nextInt();
+
+        int sumaFecha = dia + mes + anio;
+        int dig1 = sumaFecha % 10;
+        sumaFecha /= 10;
+        int dig2 = sumaFecha % 10;
+        sumaFecha /= 10;
+        int dig3 = sumaFecha % 10;
+        sumaFecha /= 10;
+        int dig4 = sumaFecha % 10;
+
+        int luckyNum = dig1 + dig2 + dig3 + dig4;
+        System.out.println("Tu numero de la suerte es: "+luckyNum);
+    }
 }
