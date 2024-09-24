a6e2d48ae17b5c28de6e45bb88968cfdd9a5e960
FersaFzs <fersafzs04@gmail.com>
2024-09-24 16:56:39 +0200

    Archivo primeros ejercicios

---
+++ Ejercicios1.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class Ejercicios1 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Ejercicio 1
+        System.out.print("Introduce el radio del circulo: ");
+        float r = scanner.nextFloat();
+        final double pi = 3.1415 ;
+        double area = pi * (r*r);
+        double perimetro = 2 * pi * r;
+        System.out.println("El area del circulo es : "+area+"\nEl perimetro del circulo es : "+perimetro);
+
+        //Ejercicio 2
+        System.out.print("Introduce 4 enteros: ");
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int n3 = scanner.nextInt();
+        int n4 = scanner.nextInt();
+
+        int multiplicacion = n1 * n2 * n3 * n4;
+        int suma = n1+n2+n3+n4;
+        double media = suma / 4.0;
+
+        System.out.println("El producto de los numeros introducidos es: "+multiplicacion+" la suma es: "+suma+" la media es: "+media);
+
+        //Ejercicio 3
+        System.out.print("Introduce el numero total de ventas: ");
+        int ventas = scanner.nextInt();
+        System.out.print("Introduce el numero de productos electrónicos vendidos:");
+        int electronicos = scanner.nextInt();
+        int ropa = ventas - electronicos;
+        double porcentajeElectronicos = (electronicos * 100.0) / ventas;
+        double porcentajeRopa = (ropa * 100.0) / ventas;
+
+        System.out.println("Has vendido "+ventas+" productos, de los cuales el "+porcentajeElectronicos+"% son productos electronicos y el otro "+porcentajeRopa+"% es ropa.");
+
+        //Ejercicio 4
+        int a=3,b=4,c=5;
+        int x;
+        x = b;
+        b = a;
+        a = c;
+        c = x;
+
+        //Ejercicio 4.1
+        a = 6;
+        b = 7;
+        c = 8;
+        x = 0;
+        int d = 9;
+        x = b; //Guardo el valor de B
+        b = a; //B toma el valor de A
+        a = d; //Guardo el valor de D
+        d = x; //D toma el valor de B
+        x = c; //Guardo el valor de C
+        c = a; //C toma el valor de D
+        a = x; //A toma el valor de C
+
+        //Ejercicio 5
+
+
+
+
+        //Ejercicio 6
+    }
+}
