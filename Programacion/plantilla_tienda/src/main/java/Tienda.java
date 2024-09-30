5374988634806c925961ad27346423c00c24437f
FersaFzs <fersafzs04@gmail.com>
2024-09-30 18:31:39 +0200

    Plantilla tienda

---
+++ Programacion/plantilla_tienda/src/main/java/Tienda.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Tienda {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+        //CON IDEA DE ORGANIZAR Y SEPARAR EL CODIGO
+        //DEFINIR LAS VARIABLES (FINAL SI PROCEDE) NECESARIAS AQUI ARRIBA
+        //%DEL IVA ... ETC
+        //PRECIOS DE LOS PRODUCTOS NOMBRES DE LOS PRODUCTOS,ETC...
+        //VARIABLES PARA EL DESCUENTO ALEATORIO
+
+        //=====================================================================
+
+        //ASIGNAR EL NOMBRE DE LOS PRODUCTOS
+
+        //ASIGNAR EL PRECIO DE LOS PRODUCTOS
+
+        //=====================================================================
+        //IMPRIMIR EL MENSAJE DE BIENVENIDA
+
+        //IMPRIMIR EL LOGOTIPO
+
+        //IMPRIMIR TABLA DE PRODUCTOS CON SUS PRECIOS
+
+
+        //PEDIR LA CANTIDAD QUE EL CLIENTE QUIERE COMPRAR DE CADA PRODUCTO
+        //SUPONEMOS QUE EL CLIENTE NO ESCRIBE NUMEROS NEGATIVOS
+
+        //PREGUNTAR EL NOMBRE AL CLIENTE PARA LA FACTURA
+
+        //====================================================================
+        //CALCULAR TODO LO NECESARIO DE LA FORMA QUE ESTIMEIS OPORTUNA
+
+        //====================================================================
+        //SR/SRA AQUI TIENE SU FACTURA
+        //MOSTRAR EL RESUMEN DE LA FACTURA
+        //NOMBRE DEL CLIENTE
+        //CUANTO COMPRÓ DE CADA PRODUCTO
+        //TOTAL SIN IVA
+        ///%IVA APLICADO
+        //EL IVA QUE SE APLICA AL TOTAL EN BASE AL PORCENTAJE ANTERIOR
+        //TOTAL CON IVA
+        //DESCUENTO ALEATORIO
+        //DESCUENTO APLICADO
+        //PRECIO FINAL
+
+        //IMPRIMIR MENSAJE DE DESPEDIDA
+    }
+}
