e2ef0ea0b25443456cf2281ae98372184be47874
FersaFzs <fersafzs04@gmail.com>
2024-11-18 19:57:29 +0100

    Ejercicios programacion y examen html

--- Programacion/EjercicioHerencia/src/Lavadora.java
+++ Programacion/EjercicioHerencia/src/Lavadora.java
@@ -6,5 +6,27 @@ public class Lavadora extends Electrodomestico{
         this.carga = capacidad;
     }
 
+    public Lavadora(){
+        super();
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "Carga: "+ getCarga();
+    }
+
+    @Override
+    public double precioFinal() {
+        double precioFinal = super.precioFinal();
+        if(this.carga > 6){
+        precioFinal += 50;
+        }
+        return precioFinal;
+    }
+
 
 }
